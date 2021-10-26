f37bdcb24091236bf2f3291b835096d5c0e03f93
Rasel Ahmed <43931972+rashelahmed049@users.noreply.github.com>
2021-10-26 22:16:53 +0530

    Add files via upload

---
+++ Integer_to_roman.py
@@ -0,0 +1,15 @@
+def roman(n):
+    d = [["I",1],["IV",4],["V",5],["IX",9],["X",10],["XL",40],["L",50],["XC",90],["C",100],
+    ["CD",400],["D",500],["CM",900],["M",100]]
+    res=""
+    for sys,v in reversed(d):
+        if n//v:
+            c = n//v
+            res+=sys*c
+            n = n%v
+        if n==0:
+            break
+    return res
+
+n = 6
+print(roman(6))
